cc79200f49a56e705cdf8134c14303d2974959c4
Andrei Jiroh Halili <ajhalili2006@crew.recaptime.eu.org>
2023-10-28 03:32:53 +0000

    Format logs and hardcode WORKDIR a bit
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@crew.recaptime.eu.org>

--- .github/actions/mkdocs-build/entrypoint.sh
+++ .github/actions/mkdocs-build/entrypoint.sh
@@ -1,3 +1,27 @@
-#!/usr/bin/env bash -l
+#!/usr/bin/env -S bash -l
 
-MKDOCS_CONFIG_PATH="/github/workspace/${1:-"mkdocs.yml}"
+WORKSPACE_DIR=${GITHUB_WORKSPACE}
+MKDOCS_CONFIG_PATH="/github/workspace/${1}"
+
+echo "::group::installing project dependencies"
+if [ -f "$GITHUB_WORKSPACE/Pipfile.lock" ]; then
+  pipenv install --ignore-pipfile --deploy
+else
+  pip3 install -r requirements.txt
+echo "::endgroup::"
+
+echo "::group::running build"
+if [ -f "$GITHUB_WORKSPACE/Pipfile.lock" ]; then
+  pipenv run mkdocs build -f "${MKDOCS_CONFIG_PATH}" -d site
+else
+  mkdocs build -f "${MKDOCS_CONFIG_PATH}" -d site
+fi
+echo "::endgroup::"
+
+echo "::group::generating archive"
+tar -C site -cvz . -f build-result.tar.gz
+echo "::endgroup::"
+
+echo "::group:: cleanup"
+rm -rv site
+echo "::endgroup::"
